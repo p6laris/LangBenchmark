b4c5e1e23c47e4e6b3efe54a29662dbb47213ab0
p6laris <yunissurchy@gmail.com>
2024-11-29 22:07:00 +0300

    Add project files.

---
+++ CsharpBenchmakrs.cs
@@ -0,0 +1,30 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Jobs;
+
+namespace LangBenchmarks
+{
+    public class CsharpBenchmarks
+    {
+        private static readonly Random Random = new Random();
+
+        [Benchmark]
+        public int Loop()
+        {
+            int u = int.Parse("40");
+            int r = Random.Next(0,10_000);
+
+            Span<int> a = stackalloc int[10_000];
+
+            for (int i = 0; i < 10_000; i++)
+            {
+                for (int j = 0; j < 100_000; j++)
+                {
+                    a[i] += j % u;
+                }
+                a[i] += r;
+            }
+
+            return a[r];
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Configs;
+using BenchmarkDotNet.Environments;
+using BenchmarkDotNet.Jobs;
+using BenchmarkDotNet.Running;
+using LangBenchmarks;
+
+namespace LangBenchmark
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = DefaultConfig.Instance
+                .AddJob(Job.Default.WithRuntime(NativeAotRuntime.Net90))
+                .AddJob(Job.Default.WithRuntime(CoreRuntime.Core90)); 
+
+            BenchmarkSwitcher
+                .FromAssembly(typeof(Program).Assembly)
+                .Run(args, config);
+        }
+    }
+}
